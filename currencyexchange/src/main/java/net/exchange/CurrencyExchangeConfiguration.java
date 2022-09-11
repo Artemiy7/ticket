a61c374b32fe9325c516276fd272d8300f9db5f0
artemiy <artemsemeniuk77@gmail.com>
2022-09-11 11:37:22 +0300

    Added CurrencyExchangeConfiguration currencyExchange

---
+++ currencyexchange/src/main/java/net/exchange/CurrencyExchangeConfiguration.java
@@ -0,0 +1,30 @@
+package net.exchange;
+
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@SpringBootApplication
+public class CurrencyExchangeConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeConfiguration.class, args);
+    }
+
+    @Bean
+    public RestTemplate createRestTemplate(){
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+}
