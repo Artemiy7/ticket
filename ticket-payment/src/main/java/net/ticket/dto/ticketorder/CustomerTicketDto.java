a3563a0581e91a100bea25c5b54d0e1647d16cc4
a1 <Artemiy7>
2023-01-27 10:41:02 +0200

    changed TicketOrderDtos

---
+++ ticket-payment/src/main/java/net/ticket/dto/ticketorder/CustomerTicketDto.java
@@ -0,0 +1,34 @@
+package net.ticket.dto.ticketorder;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import net.ticket.entity.occasion.OccasionSeatEntity;
+import net.ticket.enums.ticket.SeatPlaceType;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class CustomerTicketDto {
+    private long customerTicketId;
+    @NonNull
+    private BigDecimal amount;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @NonNull
+    private String country;
+    @NonNull
+    private short seat;
+    @NonNull
+    private SeatPlaceType seatPlaceType;
+    @JsonBackReference
+    private TicketOrderDto ticketOrderDto;
+    @JsonIgnore
+    private OccasionSeatEntity occasionSeat;
+}
---
+++ ticket-payment/src/main/java/net/ticket/dto/ticketorder/TicketOrderDto.java
@@ -0,0 +1,34 @@
+package net.ticket.dto.ticketorder;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
+import net.ticket.enums.ticket.TicketType;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class TicketOrderDto {
+    @NonNull
+    private TicketType ticketType;
+    @NonNull
+    @JsonManagedReference
+    private Set<CustomerTicketDto> customerTicketDto;
+    @NonNull
+    private String occasionAddress;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @NonNull
+    private LocalDateTime occasionDate;
+    @NonNull
+    private String occasionName;
+    @NonNull
+    private long bankAccount;
+    @NonNull
+    private String currency;
+}
+
