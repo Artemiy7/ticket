6242c3d1f62525421cefbfe138ea1429dc4680a1
a1 <Artemiy7>
2023-01-30 18:06:29 +0200

    removed ApiModelProperty

--- ticket-payment/src/main/java/net/ticket/enums/filtertype/OccasionFilterType.java
+++ ticket-payment/src/main/java/net/ticket/enums/filtertype/OccasionFilterType.java
@@ -1,33 +1,24 @@
 package net.ticket.enums.filtertype;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
 
 public enum OccasionFilterType {
-    @ApiModelProperty("TICKET_TYPE")
     @JsonProperty("TICKET_TYPE")
     TICKET_TYPE("ticketTypeOccasionFilter"),
-    @ApiModelProperty("OCCASION_DATE_FROM")
     @JsonProperty("OCCASION_DATE_FROM")
     OCCASION_DATE_FROM("occasionDateFromOccasionFilter"),
-    @ApiModelProperty("OCCASION_DATE_TO")
     @JsonProperty("OCCASION_DATE_TO")
     OCCASION_DATE_TO("occasionDateToOccasionFilter"),
-    @ApiModelProperty("OCCASION_NAME")
     @JsonProperty("OCCASION_NAME")
     OCCASION_NAME("occasionNameOccasionFilter"),
-    @ApiModelProperty("NOT_BOOKED_SEATS_FROM")
     @JsonProperty("NOT_BOOKED_SEATS_FROM")
     NOT_BOOKED_SEATS_FROM("notBookedSeatsFromOccasionFilter"),
-    @ApiModelProperty("NOT_BOOKED_SEATS_TO")
     @JsonProperty("NOT_BOOKED_SEATS_TO")
     NOT_BOOKED_SEATS_TO("notBookedSeatsToOccasionFilter"),
-    @ApiModelProperty("NUMBER_OF_SEATS_FROM")
     @JsonProperty("NUMBER_OF_SEATS_FROM")
     NUMBER_OF_SEATS_FROM("numberOfSeatsFromOccasionFilter"),
-    @ApiModelProperty("NUMBER_OF_SEATS_TO")
     @JsonProperty("NUMBER_OF_SEATS_TO")
     NUMBER_OF_SEATS_TO("numberOfSeatsToOccasionFilter");
 
