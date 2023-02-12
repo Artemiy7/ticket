1ee7a004487a916d14931e8827cd3404f32f9ba3
a1 <Artemiy7>
2023-02-12 19:59:42 +0200

    changed config server

--- config-server/src/main/java/net/ticket/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/net/ticket/configserver/ConfigServerApplication.java
@@ -1,13 +1,23 @@
 package net.ticket.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
+//    @Value("${server.timeZone}")
+    private String timeZone = "UTC";
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
