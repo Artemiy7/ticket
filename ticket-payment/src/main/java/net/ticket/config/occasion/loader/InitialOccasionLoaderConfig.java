fba356fab4772770f410c89795e2d702aeee5d0e
a1 <Artemiy7>
2023-02-13 14:55:06 +0200

    ticket-payment add configuration occasionLoaderDtoList #4

---
+++ ticket-payment/src/main/java/net/ticket/config/occasion/loader/InitialOccasionLoaderConfig.java
@@ -0,0 +1,16 @@
+package net.ticket.config.occasion.loader;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "service.occasions")
+public class InitialOccasionLoaderConfig {
+    private List<String> occasionLoaderDtoList;
+}
