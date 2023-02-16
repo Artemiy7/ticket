af189ccdcec246bc4b1b6bf993e462962dba61d9
a1 <Artemiy7>
2023-02-16 22:53:20 +0200

    ticket-payment: fix SeatPlaceType enum for config #20

--- ticket-payment/src/main/java/net/ticket/enums/ticket/SeatPlaceType.java
+++ ticket-payment/src/main/java/net/ticket/enums/ticket/SeatPlaceType.java
@@ -11,25 +11,25 @@ public enum SeatPlaceType {
 
     //net.ticket.service.occasion.cost.concert.ConcertClubOccasionCost
     @JsonProperty("CONCERT_CLUB_VIP")
-    CONCERT_CLUB_VIP("concertClubVip"),
+    CONCERT_CLUB_VIP("vip"),
     @JsonProperty("CONCERT_CLUB_FAN")
-    CONCERT_CLUB_FAN("concertClubFan"),
+    CONCERT_CLUB_FAN("fan"),
     @JsonProperty("CONCERT_CLUB_BALCONY")
-    CONCERT_CLUB_BALCONY("concertClubBalcony"),
+    CONCERT_CLUB_BALCONY("balcony"),
 
     //net.ticket.service.occasion.cost.concert.ConcertStadionOccasionCost
     @JsonProperty("CONCERT_STADION_VIP")
-    CONCERT_STADION_VIP("concertStadionVip"),
+    CONCERT_STADION_VIP("vip"),
     @JsonProperty("CONCERT_STADION_FAN")
-    CONCERT_STADION_FAN("concertStadionFan"),
+    CONCERT_STADION_FAN("fan"),
     @JsonProperty("CONCERT_STADION_SEAT")
-    CONCERT_STADION_SEAT("concertStadionSeat"),
+    CONCERT_STADION_SEAT("seating"),
 
     //net.ticket.service.occasion.cost.train.IntercityTrainCost
     @JsonProperty("TRAIN_INTERCITY_BUSINESS")
-    TRAIN_INTERCITY_BUSINESS("trainIntercityBusiness"),
+    TRAIN_INTERCITY_BUSINESS("business"),
     @JsonProperty("TRAIN_INTERCITY_ECONOMIC")
-    TRAIN_INTERCITY_ECONOMIC("trainIntercityEconomic");
+    TRAIN_INTERCITY_ECONOMIC("economic");
 
     private static final Map<String, SeatPlaceType> ENUM_MAP;
     private String seatPlaceType;
