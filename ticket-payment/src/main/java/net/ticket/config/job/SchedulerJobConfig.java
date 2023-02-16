e742e7d3b145be81af5b4a0e9f0b940b9c140995
a1 <Artemiy7>
2023-02-16 22:45:09 +0200

    ticket-payment: add shedlock job config for @Sheduled #17

---
+++ ticket-payment/src/main/java/net/ticket/config/job/SchedulerJobConfig.java
@@ -0,0 +1,35 @@
+package net.ticket.config.job;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+
+//todo take defaultLockAtMostFor out to config
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "3s")
+public class SchedulerJobConfig {
+    private final String instanceId;
+
+    public SchedulerJobConfig(@Value("${eureka.instance.metadataMap.instanceId}") String instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withLockedByValue(instanceId)
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime() // Works on Postgres, MySQL, MariaDb, MS SQL, Oracle, DB2, HSQL and H2
+                        .build()
+        );
+    }
+}
