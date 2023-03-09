6858ae9ae3a270a26584a1b7c8ae05d73839dc4a
a1 <Artemiy7>
2023-03-09 12:31:59 +0200

    ticket-payment: fix OCCASION_NAME regex constant #46

--- ticket-payment/src/main/java/net/ticket/constant/RegexConstants.java
+++ ticket-payment/src/main/java/net/ticket/constant/RegexConstants.java
@@ -1,7 +1,7 @@
 package net.ticket.constant;
 
 public class RegexConstants {
-    public final static String OCCASION_NAME = "(Concert|Train) ([a-zA-Z]+ - [a-zA-Z]+)";
+    public final static String OCCASION_NAME = "(Concert|Train) - [a-zA-Z ]+";
     public final static String OCCASION_ADDRESS = "([A-Za-z0-9 ]+)";
     public final static String CURRENCY = "(^[a-z]{3})$";
 }
