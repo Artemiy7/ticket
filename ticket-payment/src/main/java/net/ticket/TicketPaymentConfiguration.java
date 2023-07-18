fd8db8c6d427528d4637afe6afc4f60e05db8df3
a1 <Artemiy7>
2023-07-18 17:09:52 +0300

    change TicketPaymentConfiguration

--- ticket-payment/src/main/java/net/ticket/TicketPaymentConfiguration.java
+++ ticket-payment/src/main/java/net/ticket/TicketPaymentConfiguration.java
@@ -1,30 +1,19 @@
 package net.ticket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import net.ticket.config.ValidatingSerializerModifier;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 import javax.annotation.PostConstruct;
-import javax.validation.Validator;
 import java.util.TimeZone;
 
 @EnableEurekaClient
 @EnableTransactionManagement
 @SpringBootApplication//(exclude = {HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 public class TicketPaymentConfiguration {
-    @Autowired
-    Jackson2ObjectMapperBuilder builder;
     @Value("${server.timeZone}")
     private String timeZone;
     @PostConstruct
@@ -32,12 +21,6 @@ public class TicketPaymentConfiguration {
         TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
     }
 
-//    @Bean
-//    public ObjectMapper objectMapper() {
-//        return new Jackson2ObjectMapperBuilder().createXmlMapper(false).build().registerModule(new SimpleModule()
-//                .setSerializerModifier(new ValidatingSerializerModifier()));
-//
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(TicketPaymentConfiguration.class, args);
